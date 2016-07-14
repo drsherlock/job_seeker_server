8730819f5fa14db6d62f0acf1e0fbbc2604f7877
drsherlock <zain.delhi27@hotmail.com>
2016-07-14 13:35:46 +0530

    Add correct changes for Heroku

--- job_seeker/settings.py
+++ job_seeker/settings.py
@@ -138,14 +138,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_pro'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_pro', 'root_static')
 
 try:
     from .settings_local import *
