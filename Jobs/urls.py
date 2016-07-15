ada5a8c5638e618ae5dacf9c528b4ca0c05b8a83
drsherlock <zain.delhi27@hotmail.com>
2016-07-15 15:52:52 +0530

    Change api urls

--- Jobs/urls.py
+++ Jobs/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from Jobs import views
 
 urlpatterns = [
-    url(r'^companies/$', views.get_companies),
-    url(r'^jobs/$', views.get_jobs),
-    url(r'^companies/(?P<id>\d+)/$', views.get_company),
+    url(r'^companies/api/$', views.get_companies),
+    url(r'^jobs/api/$', views.get_jobs),
+    url(r'^companies/(?P<id>\d+)/api/$', views.get_company),
     # url(r'^jobs/(?P<id>\d+)/$', views.get_job)
 
 ]
