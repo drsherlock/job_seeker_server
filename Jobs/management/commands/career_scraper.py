d5fb8748991349dae58a6770b129245340511061
drsherlock <zain.delhi27@hotmail.com>
2016-08-12 23:50:05 +0530

    Make corrections to career_scraper

--- Jobs/management/commands/career_scraper.py
+++ Jobs/management/commands/career_scraper.py
@@ -1,3 +1,4 @@
+from django.core.management.base import BaseCommand, CommandError
 import os
 import re
 import requests
@@ -12,60 +13,60 @@ django.setup()
 
 from Jobs.models import Company, Job
 
-
-def update_jobs(company, profiles):
-    name = company.company_name
-    if profiles is not None:
-        company.opening = True
-        company.no_openings = len(profiles)
-        for profile in profiles:
-            job = Job.objects.get_or_create(company=company, job_name=profile)
-            print name + ":" + profile
-    else:
-        company.opening = False
-        company.no_openings = 0
-        company.clear()
-        print name + ":noooooo jobs"
-    company.save()
-
-
-def find_jobs(company, soup):
-    allowed = re.compile(r"Developer|Designer|Engineer|Admin|Manager|Writer|Executive|Lead|Analyst|Editor|"
-                         r"Associate|Architect|Recruiter|Specialist|Scientist|Support|Expert|SSE|Head"
-                         r"Producer|Evangelist|Ninja", re.IGNORECASE)
-    not_allowed = ['responsibilities', 'description', 'requirements', 'experience', 'empowering', 'engineering',
-                   'find', 'skills', 'recruiterbox', 'google', 'communicating', 'associated', 'internship',
-                   'proficient', 'leadsquared', 'referral', 'should', 'must', 'become', 'global', 'degree', 'good',
-                   'capabilities', 'leadership', 'services', 'expertise', 'architecture', 'hire', 'follow',
-                   'procedures', 'conduct', 'perk', 'missed', 'generation', 'search', 'tools', 'worldwide', 'contact',
-                   'question', 'intern', 'classes', 'trust', 'ability', 'businesses', 'join', 'industry', 'response', 'you', 'using', 'work', 			   'based', 'grow', 'provide']
-
-    profile_list = set()
-    k = soup.body.findAll(text=allowed)
-    for i in k:
-        if len(i) < 60 and not any(x in i.lower() for x in not_allowed):
-            profile_list.add(i.strip().upper())
-    update_jobs(company, profile_list)
+class Command(BaseCommand):
+	def update_jobs(self, company, profiles):
+		name = company.company_name
+		if profiles is not None:
+			company.opening = True
+			company.no_openings = len(profiles)
+			for profile in profiles:
+				job = Job.objects.get_or_create(company=company, job_name=profile)
+				print name + ":" + profile
+		else:
+			company.opening = False
+			company.no_openings = 0
+			company.clear()
+			print name + ":noooooo jobs"
+		company.save()
 
 
-def get_html(company):
-    url = company.career_url
-    try:
-    	career_page = requests.get(url)
-    except:
-	print company.company_name
-    else:
-    	soup = BeautifulSoup(career_page.text, "lxml")
-    	find_jobs(company, soup)
+	def find_jobs(self, company, soup):
+		allowed = re.compile(r"Developer|Designer|Engineer|Admin|Manager|Writer|Executive|Lead|Analyst|Editor|"
+							 r"Associate|Architect|Recruiter|Specialist|Scientist|Support|Expert|SSE|Head"
+							 r"Producer|Evangelist|Ninja", re.IGNORECASE)
+		not_allowed = ['responsibilities', 'description', 'requirements', 'experience', 'empowering', 'engineering',
+					   'find', 'skills', 'recruiterbox', 'google', 'communicating', 'associated', 'internship',
+					   'proficient', 'leadsquared', 'referral', 'should', 'must', 'become', 'global', 'degree', 'good',
+					   'capabilities', 'leadership', 'services', 'expertise', 'architecture', 'hire', 'follow',
+					   'procedures', 'conduct', 'perk', 'missed', 'generation', 'search', 'tools', 'worldwide', 'contact',
+					   'question', 'intern', 'classes', 'trust', 'ability', 'businesses', 'join', 'industry', 'response', 'you', 'using', 'work', 			   'based', 'grow', 'provide']
+
+		profile_list = set()
+		k = soup.body.findAll(text=allowed)
+		for i in k:
+			if len(i) < 60 and not any(x in i.lower() for x in not_allowed):
+				profile_list.add(i.strip().upper())
+		self.update_jobs(company, profile_list)
 
 
-def get_company():
-    companies = Company.objects.all()
-    for company in companies:
-        get_html(company)
+	def get_html(self, company):
+		url = company.career_url
+		try:
+			career_page = requests.get(url)
+		except:
+			print company.company_name
+		else:
+			soup = BeautifulSoup(career_page.text, "lxml")
+			self.find_jobs(company, soup)
 
 
-get_company()
+	def handle(self, *args, **options):
+		companies = Company.objects.all()
+		for company in companies:
+			self.get_html(company)
+
+# c = Command()
+# c.get_company()
 
 #
 # url = "https://www.xorlabs.in/jobs/"
