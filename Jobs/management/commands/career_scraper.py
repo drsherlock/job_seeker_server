2fb87ae2168712a9e35192c24564c1358e2f9e19
drsherlock <zain.delhi27@hotmail.com>
2016-08-15 02:36:31 +0530

    Update career_scraper

--- Jobs/management/commands/career_scraper.py
+++ Jobs/management/commands/career_scraper.py
@@ -47,7 +47,8 @@ class Command(BaseCommand):
 					   			 r"\bworldwide\b|\bcontact\b|\bquestion\b|\bintern\b|\bclasses\b|\btrust\b|\byou\b|"
 					   			 r"\bability\b|\bindustry\b|\bresponse\b|\bgrow\b|\bmust\b|\bheadline\b|\bfollow\b|"
 							   	 r"\busing\b|\bheader\b|\boffice\b|\bjobscore\b|\bmasthead\b|\bheading\b|\bpassed\b|"
-							   	 r"\btime\b|\bcolor\b|\bdevelop\b|\bbox\b|\bcrm\b|\bexceptional\b|\binterns\b|\blater\b", re.IGNORECASE)
+							   	 r"\btime\b|\bcolor\b|\bdevelop\b|\bbox\b|\bcrm\b|\bexceptional\b|\binterns\b|\blater\b|"
+							   	 r"\bimages\b|\bcreate\b|\bcoordinating\b|\bdelays\b|\blatest\b", re.IGNORECASE)
 
 		profile_list = set()
 		k = soup.body.findAll(text=allowed)
